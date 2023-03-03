9bcd38e950a9523bb60b6e64e5aa744a00c8b7c8
Saeed Abdulle <104086456+Saabdulle@users.noreply.github.com>
2023-03-03 13:53:52 +0000

    Update App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,21 +1,19 @@
-import React, { useState } from 'react'
-import { Routes, Route } from 'react-router-dom'
-import { SearchPage, HomePage, HeroesPage, NotFound } from '../pages'
-import { Nav } from '../components'
+import React, { useState } from "react";
+import { Routes, Route } from "react-router-dom";
+import { SearchPage, HomePage, HeroesPage, NotFound } from "../pages";
+import { Nav } from "../components";
 
 function App() {
-
   return (
     <Routes>
       <Route path="/" element={<Nav />}>
         <Route index element={<HomePage />} />
         <Route path="/search" element={<SearchPage />} />
-        <Route path="/Heroes" element={<HeroesPage />}/>
-        <Route path="*" element={<NotFound />}/>
+        <Route path="/Heroes" element={<HeroesPage />} />
+        <Route path="*" element={<NotFound />} />
       </Route>
     </Routes>
-
-  )
+  );
 }
 
-export default App
+export default App;
